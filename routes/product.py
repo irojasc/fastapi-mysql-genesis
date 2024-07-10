25873805388dd49aa72a9f61d704b5ec8ad594e9
IROJASC <irojasc3@gmail.com>
2024-07-09 23:37:27 -0500

    se agrega ruta para consulta stock por editorial

--- routes/product.py
+++ routes/product.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, HTTPException
-from sqlalchemy import select
+from fastapi.responses import JSONResponse
+from sqlalchemy import select, text
 from utils.validate_jwt import jwt_dependecy
-# from models.user import User
+from config.db import con
 
 product_route = APIRouter(
     prefix = '/product',
@@ -18,4 +19,30 @@ async def get_products(jwt_dependency: jwt_dependecy):
         }
     else:
         raise HTTPException(status_code=401, detail='Authentication failed')
+
+@product_route.get("/stock", status_code=200)
+async def get_stock_by_publisher(jwt_dependency: jwt_dependecy, Publisher: str = None):
+    query = "wp.idProduct as idProduct, pr.isbn, pr.title, pr.publisher, sum(wp.qtyNew) as stock from genesisDB.ware_product wp inner join genesisDB.product pr on wp.idProduct = pr.id where pr.publisher = \"{}\" group by  wp.idProduct , pr.isbn, pr.title, pr.publisher order by wp.idProduct asc;".format(Publisher.upper())
+    if not(jwt_dependency):
+        raise HTTPException(
+            status_code=498,
+            detail='Invalid Access Token',
+        )
+    else:
+        stock = con.execute(select(text(query)))
+        data = stock.fetchall()
+        if bool(len(data)):
+            result  = list(map(lambda x: {"id": x[0], "isbn": x[1], "title": x[2], "publisher": x[3], "stock": int(x[4])}, data))
+        else:
+            result = []
+        return JSONResponse(
+            status_code=200,
+            content={"result": result}
+            )
+    # if jwt_dependency:
+    #     return {
+    #         "content": product_list
+    #     }
+    # else:
+    #     raise HTTPException(status_code=401, detail='Authentication failed')
 
