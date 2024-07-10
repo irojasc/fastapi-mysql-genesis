922f95a7e80a2e5dccb003c6bbe20666e2d66077
IROJASC <irojasc3@gmail.com>
2024-07-09 22:10:42 -0500

    lanzamiento final

--- config/db.py
+++ config/db.py
@@ -1,9 +1,26 @@
+import os
 from sqlalchemy import create_engine, MetaData
-from decouple import Config, RepositoryEnv
-DOTENV_FILE = './.env'
-env_config = Config(RepositoryEnv(DOTENV_FILE))
-# engine = create_engine("mysql+pymysql://root:chuspa123@localhost:3306/genesisdb")
-# engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(env_config.get('MYSQL_USER'), env_config.get('MYSQL_PASSWORD'), env_config.get('MYSQL_HOST'), env_config.get('MYSQL_PORT'), env_config.get('MYSQL_DB')))
-engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE))
+engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(
+    os.getenv("DB_USER", default=None),
+    os.getenv("DB_PASSWORD", default=None),
+    os.getenv("DB_HOST", default=None),
+    os.getenv("DB_PORT", default=None),
+    os.getenv("DB_DATABASE", default=None),
+        )
+    )
 meta = MetaData()
 con = engine.connect()
+
+
+
+# from decouple import Config, RepositoryEnv
+# DOTENV_FILE = './.env'
+# env_config = Config(RepositoryEnv(DOTENV_FILE))
+# engine = create_engine("mysql+pymysql://root:chuspa123@localhost:3306/genesisdb")
+
+# engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE))
+    # env_config.get('MYSQL_USER'), 
+    # env_config.get('MYSQL_PASSWORD'), 
+    # env_config.get('MYSQL_HOST'), 
+    # env_config.get('MYSQL_PORT'), 
+    # env_config.get('MYSQL_DB'),
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import auth
 from routes.product import product_route
 from routes.user import user_route
 from routes.warehouse import warehouse_route
+import uvicorn
+import os
 
 app = FastAPI(
     title="GENESIS API",
@@ -22,3 +24,6 @@ async def default():
             "state":True,
             "message": "Wellcome to Genesis API",
             }, status_code=200)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
