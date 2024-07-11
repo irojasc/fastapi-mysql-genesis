2277e9220af3253b04a57ad04fb8d434733ade87
IROJASC <irojasc3@gmail.com>
2024-07-10 22:25:35 -0500

    termina ruta que consulta productos stock por publisher

--- routes/product.py
+++ routes/product.py
@@ -22,23 +22,42 @@ async def get_products(jwt_dependency: jwt_dependecy):
 
 @product_route.get("/stock", status_code=200)
 async def get_stock_by_publisher(jwt_dependency: jwt_dependecy, Publisher: str = None):
-    query = "wp.idProduct as idProduct, pr.isbn, pr.title, pr.publisher, sum(wp.qtyNew) as stock from genesisDB.ware_product wp inner join genesisDB.product pr on wp.idProduct = pr.id where pr.publisher = \"{}\" group by  wp.idProduct , pr.isbn, pr.title, pr.publisher order by wp.idProduct asc;".format(Publisher.upper())
     if not(jwt_dependency):
         raise HTTPException(
             status_code=498,
             detail='Invalid Access Token',
         )
     else:
-        stock = con.execute(select(text(query)))
-        data = stock.fetchall()
-        if bool(len(data)):
-            result  = list(map(lambda x: {"id": x[0], "isbn": x[1], "title": x[2], "publisher": x[3], "stock": int(x[4])}, data))
-        else:
+        if bool(Publisher):
+            query = "wp.idProduct as idProduct, pr.isbn, pr.title, pr.autor, pr.publisher, wr.code, wp.pvNew, if(exp(sum(ln(wp.isEnabled))) is not NULL, 1, 0) as enabled, sum(wp.qtyNew) as stock from genesisDB.ware_product wp inner join genesisDB.product pr on wp.idProduct = pr.id inner join genesisDB.ware wr on wp.idWare = wr.id where pr.publisher like '%{}%' group by  wp.idProduct , pr.isbn, pr.title, pr.publisher, wr.code, wp.pvNew order by wp.idProduct asc;".format(Publisher.upper())
+            stock = con.execute(select(text(query)))
+            data = stock.fetchall()
             result = []
-        return JSONResponse(
+            for item in data:
+                _index = next((index for (index, d) in enumerate(result) if d["id"] == item[0]), None)
+                if _index is None:
+                    result.append({
+                        "id": item[0],
+                        "isbn": item[1],
+                        "title": item[2],
+                        "autor": item[3],
+                        "publisher": item[4],
+                        "isEnabled": bool(item[7]),
+                        "stock": int(item[8]),
+                        "pv": {item[5]: item[6]}
+                    })
+                else:
+                    result[_index]["isEnabled"] = result[_index]["isEnabled"] or bool(item[7])
+                    result[_index]["stock"] = result[_index]["stock"] + int(item[8])
+                    result[_index]["pv"][item[5]] = item[6]
+       
+            return JSONResponse(
             status_code=200,
             content={"result": result}
             )
+        else:
+            raise HTTPException(status_code=404, detail='Nothing to show you')
+
     # if jwt_dependency:
     #     return {
     #         "content": product_list
