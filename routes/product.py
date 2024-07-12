a8deea012338bc8d9559f4b938a506a7209aa975
IROJASC <irojasc3@gmail.com>
2024-07-11 22:48:19 -0500

    termina consulta de stock por title, autor o publisher

--- routes/product.py
+++ routes/product.py
@@ -22,17 +22,24 @@ async def get_products(jwt_dependency: jwt_dependecy):
         raise HTTPException(status_code=401, detail='Authentication failed')
 
 @product_route.get("/stock", status_code=200)
-async def get_stock_by_publisher(jwt_dependency: jwt_dependecy, Publisher: Optional[str] = None):
+async def get_stock_by_publisher(jwt_dependency: jwt_dependecy,
+                                Isbn: Optional[str] = "",
+                                Title: Optional[str] = "",
+                                Autor: Optional[str] = "",
+                                Publisher: Optional[str] = "",
+                                ):
     if not(jwt_dependency):
         raise HTTPException(
             status_code=498,
             detail='Invalid Access Token',
         )
     else:
-        if bool(Publisher):
-            query = "wp.idProduct as idProduct, pr.isbn, pr.title, pr.autor, pr.publisher, wr.code, wp.pvNew, if(exp(sum(ln(wp.isEnabled))) is not NULL, 1, 0) as enabled, sum(wp.qtyNew) as stock from genesisDB.ware_product wp inner join genesisDB.product pr on wp.idProduct = pr.id inner join genesisDB.ware wr on wp.idWare = wr.id where pr.publisher like '%{}%' group by  wp.idProduct , pr.isbn, pr.title, pr.publisher, wr.code, wp.pvNew order by wp.idProduct asc;".format(Publisher.upper())
+        if bool(Isbn) or bool(Title) or bool(Autor) or bool(Publisher):
+            query = "wp.idProduct as idProduct, pr.isbn, pr.title, pr.autor, pr.publisher, wr.code, wp.pvNew, if(exp(sum(ln(wp.isEnabled))) is not NULL, 1, 0) as enabled, sum(wp.qtyNew) as stock from genesisDB.ware_product wp inner join genesisDB.product pr on wp.idProduct = pr.id inner join genesisDB.ware wr on wp.idWare = wr.id where pr.isbn like '%{}%' and pr.title like '%{}%' and pr.autor like '%{}%' and pr.publisher like '%{}%' group by  wp.idProduct , pr.isbn, pr.title, pr.publisher, wr.code, wp.pvNew order by wp.idProduct asc;".format(Isbn.upper(), Title.upper(), Autor.upper(), Publisher.upper())
+            # query = "wp.idProduct as idProduct, pr.isbn, pr.title, pr.autor, pr.publisher, wr.code, wp.pvNew, if(exp(sum(ln(wp.isEnabled))) is not NULL, 1, 0) as enabled, sum(wp.qtyNew) as stock from genesisDB.ware_product wp inner join genesisDB.product pr on wp.idProduct = pr.id inner join genesisDB.ware wr on wp.idWare = wr.id where pr.publisher like '%{}%' and  group by  wp.idProduct , pr.isbn, pr.title, pr.publisher, wr.code, wp.pvNew order by wp.idProduct asc;".format(Publisher.upper())
             stock = con.execute(select(text(query)))
             data = stock.fetchall()
+            print(data)
             result = []
             for item in data:
                 _index = next((index for (index, d) in enumerate(result) if d["id"] == item[0]), None)
