a8bb91497c0ed163b0d05c68af29fefb5c1351b9
Ivan Rojas (irojasc) <60767922+irojasc@users.noreply.github.com>
2025-09-03 21:04:06 -0500

    Update company.py function
    se actualiza el formato de consulta

--- functions/company.py
+++ functions/company.py
@@ -1,12 +1,24 @@
-
-
 def get_all_companies(data: list = []):
     return {
-        "docNum": data[0],
-        "docName": data[1],
-        "address": data[2],
-        "email": data[3],
-        "phone": data[4],
-        "active": bool(data[6][-1]),
-        "type": data[7],
+        "CardCode": data[0],
+        "CardName": data[1],
+        "Address": data[2],
+        "E_Mail": "", #pendiente correcion
+        "Phone": "", #pendiente correcion
+        "Active": bool(data[4][-1]),
+        "CardType": data[5],
+        "LicTradNum": data[6],
+        "ContactPerson": "", #pendiente correcion
+        "City": data[13] if len(data) == 14 else None,
     }
+
+#def get_all_companies(data: list = []):
+#    return {
+#        "docNum": data[0],
+#        "docName": data[1],
+#        "address": data[2],
+#        "email": data[3],
+#        "phone": data[4],
+#        "active": bool(data[6][-1]),
+#        "type": data[7],
+#    }
