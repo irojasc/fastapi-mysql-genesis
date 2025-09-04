cd478cf924d76e7a77ed754d0e856c220209336d
Ivan Rojas (irojasc) <60767922+irojasc@users.noreply.github.com>
2025-09-03 21:13:42 -0500

    Update company.py sqlmodel
    Se actualiza el nuevo model de company

--- sqlmodel/company.py
+++ sqlmodel/company.py
@@ -4,15 +4,37 @@ from config.db import meta, engine
 
 Company = Table("company", meta, 
                 Column("cardCode", String(15), primary_key=True), 
-                Column("docName", String(50), nullable=True), 
-                Column("address", String(50), nullable=True), 
-                Column("email", String(15), nullable=True), 
-                Column("phone", String(15), nullable=True), 
+                Column("docName", String(100), nullable=True), 
+                Column("address", String(100), nullable=True), 
                 Column("idUbigeo", Integer, nullable=True),
                 Column("active", BINARY, default=b'\x01'),
                 Column("type", String(1), default='S'),
                 Column("LicTradNum", String(15), nullable=True),
-                Column("ContactPerson", String(30), nullable=True),
-                Column("creationDate", Date, nullable=False)
+                Column("creationDate", Date, nullable=True),
+                Column("DocType", String(10), nullable=True),
+                Column("CardStatus", String(100), nullable=True),
+                Column("CardCond", String(100), nullable=True),
+                Column("BusinessName", String(100), nullable=True),
+                Column("TermCode", String(10), nullable=False)
                 )
 meta.create_all(bind=engine)
+
+
+#from sqlalchemy import Table, Column
+#from sqlalchemy.sql.sqltypes import Integer, String, Date, BINARY, Boolean
+#from config.db import meta, engine
+#
+#Company = Table("company", meta, 
+#                Column("cardCode", String(15), primary_key=True), 
+#                Column("docName", String(50), nullable=True), 
+#                Column("address", String(50), nullable=True), 
+#                Column("email", String(15), nullable=True), 
+#                Column("phone", String(15), nullable=True), 
+#                Column("idUbigeo", Integer, nullable=True),
+#                Column("active", BINARY, default=b'\x01'),
+#                Column("type", String(1), default='S'),
+#                Column("LicTradNum", String(15), nullable=True),
+#                Column("ContactPerson", String(30), nullable=True),
+#                Column("creationDate", Date, nullable=False)
+#                )
+#meta.create_all(bind=engine)
